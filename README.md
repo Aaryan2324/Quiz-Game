e719d0a8781a5750569c371e6f79f66d668b93de
Aaryan2324 <129336503+Aaryan2324@users.noreply.github.com>
2024-04-13 09:56:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Nba-Quiz
+ This will be a fun quiz game for all the nba fans.
---
+++ src/questions.js
@@ -0,0 +1,57 @@
+export default [
+  {
+    id: "q1",
+    text: "Who holds the record for the most points scored in a single NBA game?",
+    answers: [
+      "Wilt Chamberlain",
+      "Kobe Bryant",
+      "Michael Jordan",
+      "Lebron James",
+    ],
+  },
+  {
+    id: "q2",
+    text: "Who is the youngest MVP (Most Valuable Player) in NBA history?",
+    answers: ["Derrick Rose", "Lebron James", "Kevin Durant", "Stephen Curry"],
+  },
+  {
+    id: "q3",
+    text: "Which NBA player holds the record for the most triple-doubles in a single season?",
+    answers: [
+      "Russell Westbrook",
+      "Oscar Robertson",
+      "Magic Johnson",
+      "Lebron James",
+    ],
+  },
+  {
+    id: "q4",
+    text: "Which NBA team is known for its Lob City era, featuring high-flying alley-oop plays?",
+    answers: [
+      "Los Angeles Clippers",
+      "Golden State Warriors",
+      "Houston Rockets",
+      "Oklahoma City Thunder",
+    ],
+  },
+  {
+    id: "q5",
+    text: "Which NBA player has won the most Defensive Player of the Year awards?",
+    answers: ["Hakeem Olajuwon", "Ben Wallace", "Tim Duncan", "Michael Jordan"],
+  },
+  {
+    id: "q6",
+    text: "Which NBA team won the most games in a single regular season?",
+    answers: [
+      "Golden State Warriors",
+      "Chicago Bulls",
+      "Los Angeles Lakers",
+      "Boston Celtics",
+    ],
+  },
+  {
+    id: "q7",
+    text: "Who was the first player to be drafted directly from high school into the NBA after the league re-introduced the rule?",
+    answers: ["Kobe Bryant", "Kevin Garnett", "Lebron James", "Dwight Howard"],
+  },
+];
